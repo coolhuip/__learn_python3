1b958d363757886b7d0129359ee48351bf1e433b
Darnel <cool.huip@gmail.com>
2022-12-26 08:23:39 -0500

    initial commit
    This repo will contain all that is learning-how-to-code.

---
+++ python_stuff/main.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from typing import Any, Optional
+
+
+
+
+
+
+# INSERT CODE HERE #
+# INSERT CODE HERE #
+# INSERT CODE HERE #
+# INSERT CODE HERE #
+# INSERT CODE HERE #
+
+
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
