aec046b0d035592b3b0ef87c3f36394dd754a87e
Danny Han <cool.huip@gmail.com>
2023-04-04 21:46:14 -0400

    routine commit

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ But, there is NO excuse for not knowing things that you should already know
 from __future__ import annotations
 from typing import Any, Optional
 
-# -------------------------- #
+# ------------------------------------- #
 # 1) Concatenating Strings
-# -------------------------- #
-
+# ------------------------------------- #
 str1 = "Hi"
 str2 = "my"
 str3 = "name"
@@ -22,7 +21,25 @@ print(str1 + ", " + str2 + " " + str3 + " " + str4 + " Slim Shady")
 print(f"{str1}, {str2} {str3} {str4} Slim Shady")
 print("{}, {} {} {} Slim Shady".format(str1, str2, str3, str4))
 
+# ------------------------------------- #
+# 2) Properly Handling Files
+# ------------------------------------- #
+filename = "my_file.txt"
+
+# BAD --> Manually opening & closing files can cause unhandled exceptions.
+f1 = open(filename, "w+")
+for i in range(3):
+    f1.write("Written by BAD practice\n")
+f1.write("\n")
+f1.close()
+
+# good --> use the "with-as" keyword.
+with open(filename, "a+") as f2:
+    for i in range(3):
+        f2.write("Appended by GOOD practice\n")
+    f2.write("\n")
+
 
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    doctest.testmod()   
