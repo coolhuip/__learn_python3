72558fd47d90c1768a10610e6668532bced707a3
Danny Han <cool.huip@gmail.com>
2023-04-04 21:23:37 -0400

    modified main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+"""
+This is going to be one giant Python3 file filled w/ code for review.
+----------------------------------------------------------------------------
+As a tech intern, ignorance is expected.
+But, there is NO excuse for not knowing things that you should already know
+  according to your resume.
+"""
 from __future__ import annotations
 from typing import Any, Optional
 
+# -------------------------- #
+# 1) Concatenating Strings
+# -------------------------- #
 
-
-x = 23
-
-
-# INSERT CODE HERE #
-# INSERT CODE HERE #
-# INSERT CODE HERE #
-# INSERT CODE HERE #
-# INSERT CODE HERE #
-
-
-
+str1 = "Hi"
+str2 = "my"
+str3 = "name"
+str4 = "is"
+# BAD
+print(str1 + ", " + str2 + " " + str3 + " " + str4 + " Slim Shady")
+# GOOD
+print(f"{str1}, {str2} {str3} {str4} Slim Shady")
+print("{}, {} {} {} Slim Shady".format(str1, str2, str3, str4))
 
 
 if __name__ == "__main__":
