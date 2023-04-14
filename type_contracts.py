a4e5d78d02798f4c62f09432b8b74c5a0f13b62f
Danny Han <cool.huip@gmail.com>
2023-04-14 06:09:33 -0400

    updated type_contracts.py

--- type_contracts.py
+++ type_contracts.py
@@ -1,4 +1,9 @@
+from __future__ import annotations
+        # The above import allows older Python versions to use updated features.
 from typing import Any, Optional, Callable, Union
+
+print("===================================")
+print()
 
 
 # You can define your own types!
@@ -12,20 +17,64 @@ def foo(v: Vector) -> Vector:
 #                        as long as there the default value is defined.
 # Optional (return type) = Function will either return 'str' or 'None'
 # ------------------------------------------------------------------------
-def funct_1(my_str: str, button: Optional[bool]=True) -> Optional[str]:
+def bar(my_str: str, button: Optional[bool]=True) -> Optional[str]:
     if button:
         return my_str
     else:
         return None
-
-print(funct_1("hello world", False))
+    
+print("DEMO: Optional type")
+print(bar("hello world", False))
+print(bar("hello world"))
+print()
 
 
 # Any = It's pretty self-explanatory.
 # -----------------------------------
-def funct_2(output: Optional[Any]=None) -> Optional[Any]:
+def funct_1(output: Optional[Any]=None) -> Optional[Any]:
     return output
 
-def funct_3(output: Any) -> Any:
+def funct_2(output: Any) -> Any:
     return output
 
+
+# Union = Define the possible types that can be passed.
+# -----------------------------------------------------
+def foo_2(x: Union[bool, int]) -> Union[list[str], str]:
+    if isinstance(x, bool):
+        return ["foo_2()", "has returned", "a", "list[str]", "type", "!"]
+    elif isinstance(x, int):
+        return "foo_2() has returned a 'str' type!"
+    else:
+        raise TypeError
+
+print("DEMO: Union type")
+print(foo_2(int()))
+print(foo_2(bool()))
+print()
+
+
+# Callable = Use this type if your parameter is a callable function.
+# ------------------------------------------------------------------
+def add(x: int, y: list[int, str]) -> int:
+    return x + y[0] + int(y[1])
+
+def add_sum(a: int, b: Callable[[int, list[int, str]], int]):
+    return a + b
+
+print("DEMO: Callable type")
+print("Result of add_sum(3, add(7, [10, '209'])) = "
+            , add_sum(3, add(7, [10, "209"])))
+
+
+
+
+
+
+print()
+print("===================================")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
